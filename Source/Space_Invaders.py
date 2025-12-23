590c4e97effe3014a2623bcb84da2ec1040f5d05
Алексей Перевалов <alexplayppap.a@gmail.com>
2025-12-23 21:37:00 +0500

    .gitignore init & add class Player

--- Source/Space_Invaders.py
+++ Source/Space_Invaders.py
@@ -23,3 +23,18 @@ clock = pygame.time.Clock()
 
 # Шрифт для счёта
 font = pygame.font.SysFont(None, 36)
+
+
+# Класс Игрока
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        # Делаем простую форму корабля
+        pygame.draw.polygon(self.image, GREEN, [(25, 0), (0, 40), (50, 40)])
+        self.rect = self.image.get_rect()
+        self.rect.centerx = SCREEN_WIDTH // 2
+        self.rect.bottom = SCREEN_HEIGHT - 10
+        self.speed = 8
+        self.lives = 3
